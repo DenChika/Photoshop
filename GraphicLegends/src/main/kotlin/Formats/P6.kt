f6ba7a15a7b087dccc162e3bbea4dd3c4fe72a1e
DenChika <eholocation@gmail.com>
2022-10-13 20:53:07 +0300

    fix: support ImageBitmap return type

--- GraphicLegends/src/main/kotlin/Formats/P6.kt
+++ GraphicLegends/src/main/kotlin/Formats/P6.kt
@@ -1,9 +1,11 @@
 package Formats
 
 import Interfaces.IFormat
+import androidx.compose.ui.graphics.ImageBitmap
 
-class P6(val fileToPath: String) : IFormat {
-    override fun Handle(width: Int, height: Int, byteArray: ByteArray) {
+class P6 : IFormat {
+    override fun Handle(width: Int, height: Int, byteArray: ByteArray) : ImageBitmap? {
         //TODO: implement parser p6 body
+        return null
     }
 }
