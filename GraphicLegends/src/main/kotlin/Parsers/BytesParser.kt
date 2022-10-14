13442657daced5706f5785de8647f0c429a30928
DenChika <eholocation@gmail.com>
2022-10-14 15:26:04 +0300

    feat: implement writer to file

--- GraphicLegends/src/main/kotlin/Parsers/BytesParser.kt
+++ GraphicLegends/src/main/kotlin/Parsers/BytesParser.kt
@@ -1,5 +1,7 @@
 package Parsers
 
+import Configuration.MutableConfigurationsState
+import Formats.Modes
 import Formats.P5
 import Formats.P6
 import androidx.compose.ui.graphics.ImageBitmap
@@ -8,7 +10,7 @@ import kotlin.math.pow
 
 class BytesParser {
     companion object {
-        fun ParseFile(file: File) : ImageBitmap? {
+        fun ParseBytesForFile(file: File) : ImageBitmap? {
             val byteArray = file.readBytes()
             if (byteArray[0].toInt().toChar() != 'P') {
                 println("not pnm format")
@@ -19,35 +21,57 @@ class BytesParser {
 
             val spaceIndex = arrayWithoutFormat.indexOf(32.toByte())
             var enterIndex = arrayWithoutFormat.indexOf(10.toByte())
-            val width = ParseValue(arrayWithoutFormat.slice(0 until spaceIndex).toByteArray())
-            val height = ParseValue(arrayWithoutFormat.slice(spaceIndex + 1 until enterIndex).toByteArray())
+            val width = ParseBytesForValue(arrayWithoutFormat.slice(0 until spaceIndex).toByteArray())
+            val height = ParseBytesForValue(arrayWithoutFormat.slice(spaceIndex + 1 until enterIndex).toByteArray())
             val bodyWithShadeValue = arrayWithoutFormat.slice(enterIndex + 1 until arrayWithoutFormat.size).toByteArray()
 
             enterIndex = bodyWithShadeValue.indexOf(10.toByte())
-            val maxShadeValue = ParseValue(bodyWithShadeValue.slice(0 until enterIndex).toByteArray())
+            val maxShadeValue = ParseBytesForValue(bodyWithShadeValue.slice(0 until enterIndex).toByteArray())
 
             val body = bodyWithShadeValue.slice(enterIndex + 1 until bodyWithShadeValue.size).toByteArray()
 
             when(byteArray[1].toInt().toChar()) {
                 '5' -> {
                     val handler = P5()
-                    return handler.Handle(width, height, body)
+                    return handler.HandleReader(width, height, maxShadeValue, body)
                 }
                 '6' -> {
                     val handler = P6()
-                    return handler.Handle(width, height, body)
+                    return handler.HandleReader(width, height, maxShadeValue, body)
                 }
             }
             return null
         }
 
-        private fun ParseValue(byteArray: ByteArray) : Int {
-            byteArray.reverse()
-            var height = 0
-            for (i in byteArray.indices) {
-                height += (byteArray[i].toInt() - '0'.code) * 10.0.pow(i).toInt()
+        fun ParseFileInBytes(path: String, width: Int, height: Int, maxShade: Int, byteArray: ByteArray?) {
+            val file = File(path)
+            File(path).createNewFile()
+            when(MutableConfigurationsState.mode) {
+                Modes.P5 -> {
+                    P5().HandleWriter(width, height, maxShade, byteArray)?.let { file.writeBytes(it) }
+                }
+                Modes.P6 -> {
+                    P6().HandleWriter(width, height, maxShade, byteArray)?.let { file.writeBytes(it) }
+                }
             }
-            return height
+        }
+        fun ParseValueForBytes(value: Int) : ByteArray {
+            var newByteArray = byteArrayOf()
+            var valueCopy = value
+            while(valueCopy != 0) {
+                newByteArray += ((valueCopy % 10) + '0'.code).toByte()
+                valueCopy /= 10
+            }
+            newByteArray.reverse()
+            return newByteArray
+        }
+        private fun ParseBytesForValue(byteArray: ByteArray) : Int {
+            byteArray.reverse()
+            var value = 0
+            for (i in byteArray.indices) {
+                value += (byteArray[i].toInt() - '0'.code) * 10.0.pow(i).toInt()
+            }
+            return value
         }
     }
 
