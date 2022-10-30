9c18d508573d5585e121e6f158133bc3c5940e47
DenChika <eholocation@gmail.com>
2022-10-31 01:19:38 +0300

    init: added the core to the lab2

---
+++ GraphicLegends/src/main/kotlin/ColorSpaces/HSL.kt
@@ -0,0 +1,11 @@
+package ColorSpaces
+
+import Interfaces.ColorSpace
+import androidx.compose.ui.graphics.ImageBitmap
+
+class HSL : ColorSpace {
+    override fun ToRGB(bodyArray: DoubleArray) : ImageBitmap? {
+        //TODO implement HSL space converter
+        return null
+    }
+}
