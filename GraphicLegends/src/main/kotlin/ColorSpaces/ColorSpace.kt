d469fbc3229d09012464a4295d2d622658234745
Dmitry Kapanin <crazyyeti@mail.ru>
2022-11-07 17:29:28 +0300

    feat: reformat logic of color space enum

--- GraphicLegends/src/main/kotlin/ColorSpaces/ColorSpace.kt
+++ GraphicLegends/src/main/kotlin/ColorSpaces/ColorSpace.kt
@@ -4,43 +4,72 @@ import Interfaces.IColorSpace
 
 enum class ColorSpace {
     RGB {
-        override fun GetInstance(firstValue: Int, secondValue: Int, thirdValue: Int): IColorSpace {
-            return RGB(firstValue, secondValue, thirdValue)
+        override fun GetDefault(): ColorSpaceInstance {
+            return ColorSpaceInstance(RGB)
+        }
+
+        override fun GetService(): IColorSpace {
+            return RGB()
         }
     },
     HSL {
-        override fun GetInstance(firstValue: Int, secondValue: Int, thirdValue: Int): IColorSpace {
-            TODO("Not yet implemented")
+        override fun GetDefault(): ColorSpaceInstance {
+            return ColorSpaceInstance(HSL)
+        }
+
+        override fun GetService(): IColorSpace {
+            return HSL()
         }
     },
     HSV {
-        override fun GetInstance(firstValue: Int, secondValue: Int, thirdValue: Int): IColorSpace {
-            TODO("Not yet implemented")
+        override fun GetDefault(): ColorSpaceInstance {
+            return ColorSpaceInstance(HSV)
+        }
+
+        override fun GetService(): IColorSpace {
+            return HSV()
         }
     },
     YCbCr601 {
-        override fun GetInstance(firstValue: Int, secondValue: Int, thirdValue: Int): IColorSpace {
-            TODO("Not yet implemented")
+        override fun GetDefault(): ColorSpaceInstance {
+            return ColorSpaceInstance(YCbCr601)
+        }
+
+        override fun GetService(): IColorSpace {
+            return YCbCr601()
         }
 
     },
     YCbCr709 {
-        override fun GetInstance(firstValue: Int, secondValue: Int, thirdValue: Int): IColorSpace {
-            TODO("Not yet implemented")
+        override fun GetDefault(): ColorSpaceInstance {
+            return ColorSpaceInstance(YCbCr709)
+        }
+
+        override fun GetService(): IColorSpace {
+            return YCbCr709()
         }
 
     },
     YCoCg {
-        override fun GetInstance(firstValue: Int, secondValue: Int, thirdValue: Int): IColorSpace {
-            TODO("Not yet implemented")
+        override fun GetDefault(): ColorSpaceInstance {
+            return ColorSpaceInstance(YCoCg)
+        }
+
+        override fun GetService(): IColorSpace {
+            return YCoCg()
         }
 
     },
     CMY {
-        override fun GetInstance(firstValue: Int, secondValue: Int, thirdValue: Int): IColorSpace {
-            TODO("Not yet implemented")
+        override fun GetDefault(): ColorSpaceInstance {
+            return ColorSpaceInstance(CMY)
+        }
+
+        override fun GetService(): IColorSpace {
+            return CMY()
         }
     };
 
-    abstract fun GetInstance(firstValue: Int, secondValue : Int, thirdValue: Int) : IColorSpace
+    abstract fun GetDefault() : ColorSpaceInstance
+    abstract fun GetService() : IColorSpace
 }
