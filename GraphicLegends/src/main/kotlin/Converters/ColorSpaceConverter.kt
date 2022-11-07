1ad454fedad5e0f6f01cc48ec4727f6de2413da8
Dmitry Kapanin <crazyyeti@mail.ru>
2022-11-07 17:30:24 +0300

    feat: add color space converter

---
+++ GraphicLegends/src/main/kotlin/Converters/ColorSpaceConverter.kt
@@ -0,0 +1,23 @@
+package Converters
+
+import ColorSpaces.ColorSpace
+import Tools.ColorSpaceException
+
+class ColorSpaceConverter {
+    companion object {
+        fun convert(start : ColorSpace, end : ColorSpace, value : DoubleArray) : DoubleArray
+        {
+            return when(end) {
+                ColorSpace.CMY -> start.GetService().ToCMY(value)
+                ColorSpace.YCoCg -> start.GetService().ToYCoCg(value)
+                ColorSpace.YCbCr601 -> start.GetService().ToYCbCr601(value)
+                ColorSpace.YCbCr709 -> start.GetService().ToYCbCr709(value)
+                ColorSpace.HSV -> start.GetService().ToHSV(value)
+                ColorSpace.HSL -> start.GetService().ToRGB(value)
+                ColorSpace.RGB -> start.GetService().ToRGB(value)
+                else -> throw ColorSpaceException.undefined()
+            }
+        }
+    }
+
+}
