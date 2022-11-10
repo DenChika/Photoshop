1162744552e1dcadf9dce2d03811b5cafea347ab
DenChika <eholocation@gmail.com>
2022-11-11 00:03:31 +0300

    feat: YCbCr601 and YCbCr709 converting to RGB and back

--- GraphicLegends/src/main/kotlin/ColorSpaces/YCbCr601.kt
+++ GraphicLegends/src/main/kotlin/ColorSpaces/YCbCr601.kt
@@ -5,11 +5,16 @@ import Interfaces.IColorSpace
 class YCbCr601 : IColorSpace {
 
     override fun ToRGB(values: FloatArray): FloatArray {
-        TODO("Not yet implemented")
+        val a = 0.299F; val b = 0.587F; val c = 0.114F; val d = 1.772F; val e = 1.402F
+        val R = values[0] + e * (values[2] - 0.5F)
+        val G = values[0] - (a * e / b) * (values[2] - 0.5F) - (c * d / b) * (values[1] - 0.5F)
+        val B = values[0] + d * (values[1] - 0.5F)
+        return floatArrayOf(R, G, B)
     }
 
     override fun ToCMY(values: FloatArray): FloatArray {
-        TODO("Not yet implemented")
+        val rgbValues = ToRGB(values)
+        return ColorSpace.RGB.GetService().ToCMY(rgbValues)
     }
 
     override fun ToHSL(values: FloatArray): FloatArray {
@@ -25,7 +30,8 @@ class YCbCr601 : IColorSpace {
     }
 
     override fun ToYCbCr709(values: FloatArray): FloatArray {
-        TODO("Not yet implemented")
+        val rgbValues = ToRGB(values)
+        return ColorSpace.RGB.GetService().ToYCbCr709(rgbValues)
     }
 
     override fun ToYCoCg(values: FloatArray): FloatArray {
