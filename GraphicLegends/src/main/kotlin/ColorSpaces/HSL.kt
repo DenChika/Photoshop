4daa22d7ec6df46cb74cfa3d2fd6f6a7831c69a9
GTEgorss <noartu@gmail.com>
2022-11-11 01:49:39 +0300

    feat: RGB <-> HSL

--- GraphicLegends/src/main/kotlin/ColorSpaces/HSL.kt
+++ GraphicLegends/src/main/kotlin/ColorSpaces/HSL.kt
@@ -1,11 +1,43 @@
 package ColorSpaces
 
 import Interfaces.IColorSpace
+import Tools.ColorSpaceException
+import kotlin.math.abs
 
 class HSL : IColorSpace {
 
     override fun ToRGB(values: FloatArray): FloatArray {
-        TODO("Not yet implemented")
+        val diff = values[1] * (1 - abs(2 * values[2] - 1))
+        val m = 255 * (values[2] - diff / 2)
+        val x = diff * (1 - abs(values[0] / 60 % 2 - 1))
+
+
+        when (values[0]) {
+            in 0f..60f -> {
+                return floatArrayOf(diff + m / 255, x + m / 255, m / 255)
+            }
+
+            in 60f..120f -> {
+                return floatArrayOf(x + m / 255, diff + m / 255, m / 255)
+            }
+
+            in 120f..180f -> {
+                return floatArrayOf(m / 255, diff + m / 255, x + m / 255)
+            }
+
+            in 180f..240f -> {
+                return floatArrayOf(m / 255, x + m / 255, diff + m / 255)
+            }
+
+            in 240f..300f -> {
+                return floatArrayOf(x + m / 255, m / 255, diff + m / 255)
+            }
+
+            in 300f..360f -> {
+                return floatArrayOf(diff + m / 255, m / 255, x + m / 255)
+            }
+            else -> throw ColorSpaceException("Error. Wrong HSL format.")
+        }
     }
 
     override fun ToCMY(values: FloatArray): FloatArray {
--- GraphicLegends/src/main/kotlin/ColorSpaces/RGB.kt
+++ GraphicLegends/src/main/kotlin/ColorSpaces/RGB.kt
@@ -1,6 +1,9 @@
 package ColorSpaces
 
 import Interfaces.IColorSpace
+import kotlin.math.abs
+import kotlin.math.acos
+import kotlin.math.sqrt
 
 class RGB() : IColorSpace {
 
@@ -16,7 +19,29 @@ class RGB() : IColorSpace {
     }
 
     override fun ToHSL(values: FloatArray): FloatArray {
-        TODO("Not yet implemented")
+        val max = maxOf(values[0], maxOf(values[1], values[2]))
+        val min = minOf(values[0], minOf(values[1], values[2]))
+        val diff = max - min
+
+        val lightness = (max + min) / 2
+
+        var saturation = 0f
+
+        if (lightness != 0f) {
+            saturation = diff / (1 - abs(2 * lightness - 1))
+        }
+
+        val r = values[0] * 255
+        val g = values[1] * 255
+        val b = values[2] * 255
+
+        val hue = if (g >= b) {
+            acos((r - g / 2 - b / 2 ) / sqrt(r * r + g * g + b * b - r * g - r * b - g * b)) / Math.PI.toFloat() * 180f
+        } else {
+            360 - acos((r - g / 2 - b / 2 ) / sqrt(r * r + g * g + b * b - r * g - r * b - g * b)) / Math.PI.toFloat() * 180f
+        }
+
+        return floatArrayOf(hue, saturation, lightness)
     }
 
     override fun ToHSV(values: FloatArray): FloatArray {
