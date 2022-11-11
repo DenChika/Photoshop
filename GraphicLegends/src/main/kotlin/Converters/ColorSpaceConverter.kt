b212b72214a9641098f0826cc9c7a905d22b1edf
GTEgorss <noartu@gmail.com>
2022-11-11 13:04:55 +0300

    fix: added ToHSL refactor: placed color spaces in the right order

--- GraphicLegends/src/main/kotlin/Converters/ColorSpaceConverter.kt
+++ GraphicLegends/src/main/kotlin/Converters/ColorSpaceConverter.kt
@@ -8,16 +8,15 @@ class ColorSpaceConverter {
         fun convert(spaceFrom : ColorSpace, spaceTo : ColorSpace, value : FloatArray) : FloatArray
         {
             return when(spaceTo) {
+                ColorSpace.RGB -> spaceFrom.GetService().ToRGB(value)
                 ColorSpace.CMY -> spaceFrom.GetService().ToCMY(value)
-                ColorSpace.YCoCg -> spaceFrom.GetService().ToYCoCg(value)
+                ColorSpace.HSL -> spaceFrom.GetService().ToHSL(value)
+                ColorSpace.HSV -> spaceFrom.GetService().ToHSV(value)
                 ColorSpace.YCbCr601 -> spaceFrom.GetService().ToYCbCr601(value)
                 ColorSpace.YCbCr709 -> spaceFrom.GetService().ToYCbCr709(value)
-                ColorSpace.HSV -> spaceFrom.GetService().ToHSV(value)
-                ColorSpace.HSL -> spaceFrom.GetService().ToRGB(value)
-                ColorSpace.RGB -> spaceFrom.GetService().ToRGB(value)
+                ColorSpace.YCoCg -> spaceFrom.GetService().ToYCoCg(value)
                 else -> throw ColorSpaceException.undefined()
             }
         }
     }
-
 }
