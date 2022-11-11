838ecf3caffea22d3cfbb73aaa2833a66b02077d
Dmitry Kapanin <crazyyeti@mail.ru>
2022-11-11 22:32:09 +0300

    chore: move color space to Space Configuration

--- GraphicLegends/src/main/kotlin/Configurations/SpaceConfiguration.kt
+++ GraphicLegends/src/main/kotlin/Configurations/SpaceConfiguration.kt
@@ -1,8 +1,20 @@
 package Configurations
 
+import ColorSpaces.ColorSpace
+import Filtration.FiltrationMode
 import androidx.compose.runtime.mutableStateOf
 
 class SpaceConfiguration {
     val expanded = mutableStateOf(false)
-    val selected = mutableStateOf("RGB")
+    private val _selected = mutableStateOf(ColorSpace.RGB)
+    var selected: ColorSpace
+        get() {
+            return _selected.value
+        }
+        set(value) {
+            _selected.value = value
+            AppConfiguration.Image.changeColorSpace(value)
+            AppConfiguration.Component.selected = FiltrationMode.ALL
+            AppConfiguration.updateBitmap()
+        }
 }
