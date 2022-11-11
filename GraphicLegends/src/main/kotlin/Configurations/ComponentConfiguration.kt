7167541360f79306209de86283fdc0bda53c74ee
DenChika <eholocation@gmail.com>
2022-11-11 19:00:58 +0300

    fix: set default true value of channel filters

--- GraphicLegends/src/main/kotlin/Configurations/ComponentConfiguration.kt
+++ GraphicLegends/src/main/kotlin/Configurations/ComponentConfiguration.kt
@@ -4,7 +4,7 @@ import androidx.compose.runtime.mutableStateOf
 
 class ComponentConfiguration {
     val expanded = mutableStateOf(false)
-    val checkedState1 = mutableStateOf(false)
-    val checkedState2 = mutableStateOf(false)
-    val checkedState3 = mutableStateOf(false)
+    val checkedState1 = mutableStateOf(true)
+    val checkedState2 = mutableStateOf(true)
+    val checkedState3 = mutableStateOf(true)
 }
