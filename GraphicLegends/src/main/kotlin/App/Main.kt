5c9ce71c0df879f37fb455b4d8d8eb457fe54a72
DenChika <eholocation@gmail.com>
2022-11-16 15:31:32 +0300

    refactor: move actions from Main.kt to separate functions

--- GraphicLegends/src/main/kotlin/App/Main.kt
+++ GraphicLegends/src/main/kotlin/App/Main.kt
@@ -1,5 +1,8 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 import App.HeadingButton
+import App.HeadingDropdownButton
+import App.OpenActivity
+import App.SaveActivity
 import ColorSpaces.ColorSpace
 import Configurations.AppConfiguration
 import Filtration.FiltrationMode
@@ -9,12 +12,9 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.awt.ComposeWindow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.painter.BitmapPainter
@@ -28,10 +28,11 @@ import java.awt.Dialog
 import java.awt.FileDialog
 import java.awt.FileDialog.SAVE
 import java.io.File
+import javax.swing.text.html.ImageView
 
 @Composable
 fun App() {
-    val appBackgroundPic = File("GraphicLegends/src/main/kotlin/Resources/app_background.jpg")
+    val appBackgroundPic = File("src/main/kotlin/Resources/app_background.jpg")
 
     val background: ImageBitmap = remember(appBackgroundPic) {
         loadImageBitmap(appBackgroundPic.inputStream())
@@ -48,124 +49,37 @@ fun App() {
             Row(Modifier.fillMaxSize()) {
                 HeadingButton(
                     onClick = {
-                        val fd = FileDialog(ComposeWindow())
-                        fd.isVisible = true
-                        if (fd.files.isNotEmpty()) {
-                            val file = fd.files[0]
-                            AppConfiguration.Image = BytesParser.ParseBytesForFile(file)!!
-                        }
+                        OpenActivity()
                     },
                     text = "Open"
                 )
 
                 HeadingButton(
                     onClick = {
-                        if (AppConfiguration.HasContent()) {
-                            val dialog: Dialog? = null
-                            val fd = FileDialog(dialog, "Write the name of file", SAVE)
-                            fd.isVisible = true
-                            if (fd.files.isNotEmpty()) {
-                                val file = fd.files[0]
-                                BytesParser.ParseFileToBytes(
-                                    file.absolutePath,
-                                    AppConfiguration.Image.width,
-                                    AppConfiguration.Image.height,
-                                    AppConfiguration.Image.maxShade
-                                )
-                            }
-                        }
+                        SaveActivity()
                     },
                     text = "Save"
                 )
                 if (AppConfiguration.HasContent()){
                     Box {
-                        Button(
-                            modifier = Modifier.padding(start = 15.dp),
+                        HeadingDropdownButton(
                             onClick = {
                                 AppConfiguration.Space.expanded.value = true
                             },
-                            colors = ButtonDefaults.buttonColors(Color.Green)
-                        ) {
-                            Text(text = AppConfiguration.Space.selected.GetName())
-                            Icon(
-                                Icons.Default.ArrowDropDown,
-                                contentDescription = "",
-                                modifier = Modifier.width(20.dp).height(20.dp)
-                            )
-                        }
-                        DropdownMenu(
-                            expanded = AppConfiguration.Space.expanded.value,
-                            onDismissRequest = { AppConfiguration.Space.expanded.value = false }
-                        ) {
-                            DropdownMenuItem(onClick = {
-                                AppConfiguration.Space.selected = ColorSpace.RGB
-                                AppConfiguration.Space.expanded.value = false
-                            }) { Text(ColorSpace.RGB.GetName()) }
-                            DropdownMenuItem(onClick = {
-                                AppConfiguration.Space.selected = ColorSpace.CMY
-                                AppConfiguration.Space.expanded.value = false
-                            }) { Text(ColorSpace.CMY.GetName()) }
-                            DropdownMenuItem(onClick = {
-                                AppConfiguration.Space.selected = ColorSpace.HSL
-                                AppConfiguration.Space.expanded.value = false
-                            }) { Text(ColorSpace.HSL.GetName()) }
-                            DropdownMenuItem(onClick = {
-                                AppConfiguration.Space.selected = ColorSpace.HSV
-                                AppConfiguration.Space.expanded.value = false
-                            }) { Text(ColorSpace.HSV.GetName()) }
-                            DropdownMenuItem(onClick = {
-                                AppConfiguration.Space.selected = ColorSpace.YCbCr601
-                                AppConfiguration.Space.expanded.value = false
-                            }) { Text(ColorSpace.YCbCr601.GetName()) }
-                            DropdownMenuItem(onClick = {
-                                AppConfiguration.Space.selected = ColorSpace.YCbCr709
-                                AppConfiguration.Space.expanded.value = false
-                            }) { Text(ColorSpace.YCbCr709.GetName()) }
-                            DropdownMenuItem(onClick = {
-                                AppConfiguration.Space.selected = ColorSpace.YCoCg
-                                AppConfiguration.Space.expanded.value = false
-                            }) { Text(ColorSpace.YCoCg.GetName()) }
-                        }
+                            text = AppConfiguration.Space.selected.GetName()
+                        )
+                        AppConfiguration.Space.DropdownSpaces()
                     }
 
                     if (AppConfiguration.Image.format != Format.P5) {
                         Box {
-                            Button(
-                                modifier = Modifier.padding(start = 15.dp),
+                            HeadingDropdownButton(
                                 onClick = {
                                     AppConfiguration.Component.expanded.value = true
                                 },
-                                colors = ButtonDefaults.buttonColors(Color.Green)
-                            ) {
-                                Text(text = AppConfiguration.Component.selected.GetName())
-                                Icon(
-                                    Icons.Default.ArrowDropDown,
-                                    contentDescription = "",
-                                    modifier = Modifier.width(20.dp).height(20.dp)
-                                )
-                            }
-                            DropdownMenu(
-                                expanded = AppConfiguration.Component.expanded.value,
-                                onDismissRequest = { AppConfiguration.Component.expanded.value = false }
-                            ) {
-                                DropdownMenuItem(onClick = {
-                                    AppConfiguration.Component.selected = FiltrationMode.ALL
-                                    AppConfiguration.Component.expanded.value = false
-                                    AppConfiguration.Component
-                                }) { Text(FiltrationMode.ALL.GetName()) }
-                                DropdownMenuItem(onClick = {
-                                    AppConfiguration.Component.selected = FiltrationMode.OnlyFirst
-                                    AppConfiguration.Component.expanded.value = false
-                                }) { Text(FiltrationMode.OnlyFirst.GetName()) }
-                                DropdownMenuItem(onClick = {
-                                    AppConfiguration.Component.selected = FiltrationMode.OnlySecond
-                                    AppConfiguration.Component.expanded.value = false
-                                }) { Text(FiltrationMode.OnlySecond.GetName()) }
-                                DropdownMenuItem(onClick = {
-                                    AppConfiguration.Component.selected = FiltrationMode.OnlyThird
-                                    AppConfiguration.Component.expanded.value = false
-                                }) { Text(FiltrationMode.OnlyThird.GetName()) }
-                            }
+                                text = AppConfiguration.Component.selected.GetName()
+                            )
+                            AppConfiguration.Component.DropdownComponents()
                         }
                     }
                 }
@@ -179,18 +93,7 @@ fun App() {
                     Card(
                         elevation = 10.dp
                     ) {
-                        AppConfiguration.GetBitmap().let {
-                            Image(
-                                bitmap = it,
-                                modifier = if (it.height > 900 && it.width > 1500) Modifier.height(700.dp)
-                                    .width(1500.dp)
-                                else if (it.height > 900) Modifier.height(700.dp)
-                                else if (it.width > 1500) Modifier.width(1500.dp)
-                                else Modifier,
-                                contentDescription = "image",
-                                contentScale = ContentScale.Crop
-                            )
-                        }
+                        AppConfiguration.Image.ImageView()
                     }
                 }
             }
--- GraphicLegends/src/main/kotlin/Configurations/SpaceConfiguration.kt
+++ GraphicLegends/src/main/kotlin/Configurations/SpaceConfiguration.kt
@@ -2,6 +2,10 @@ package Configurations
 
 import ColorSpaces.ColorSpace
 import Filtration.FiltrationMode
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 
 class SpaceConfiguration {
@@ -17,4 +21,41 @@ class SpaceConfiguration {
             AppConfiguration.Component.selected = FiltrationMode.ALL
             AppConfiguration.updateBitmap()
         }
+
+    @Composable
+    fun DropdownSpaces() {
+        DropdownMenu(
+            expanded = expanded.value,
+            onDismissRequest = { expanded.value = false }
+        ) {
+            DropdownMenuItem(onClick = {
+                selected = ColorSpace.RGB
+                expanded.value = false
+            }) { Text(ColorSpace.RGB.GetName()) }
+            DropdownMenuItem(onClick = {
+                selected = ColorSpace.CMY
+                expanded.value = false
+            }) { Text(ColorSpace.CMY.GetName()) }
+            DropdownMenuItem(onClick = {
+                selected = ColorSpace.HSL
+                expanded.value = false
+            }) { Text(ColorSpace.HSL.GetName()) }
+            DropdownMenuItem(onClick = {
+                selected = ColorSpace.HSV
+                expanded.value = false
+            }) { Text(ColorSpace.HSV.GetName()) }
+            DropdownMenuItem(onClick = {
+                selected = ColorSpace.YCbCr601
+                expanded.value = false
+            }) { Text(ColorSpace.YCbCr601.GetName()) }
+            DropdownMenuItem(onClick = {
+                selected = ColorSpace.YCbCr709
+                expanded.value = false
+            }) { Text(ColorSpace.YCbCr709.GetName()) }
+            DropdownMenuItem(onClick = {
+                selected = ColorSpace.YCoCg
+                expanded.value = false
+            }) { Text(ColorSpace.YCoCg.GetName()) }
+        }
+    }
 }
