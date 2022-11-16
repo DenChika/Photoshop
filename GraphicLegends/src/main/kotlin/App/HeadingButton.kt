218c79ff31a02d8f8380bf36b18a6ecb2f5e5b5d
DenChika <eholocation@gmail.com>
2022-11-16 15:28:47 +0300

    fix: remove checking onClick action on null

--- GraphicLegends/src/main/kotlin/App/HeadingButton.kt
+++ GraphicLegends/src/main/kotlin/App/HeadingButton.kt
@@ -1,9 +1,15 @@
 package App
 
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -11,12 +17,12 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun HeadingButton(
-    onClick: (() -> Unit)? = null,
+    onClick: () -> Unit,
     text: String = ""
 ) {
     Button(
         modifier = Modifier.padding(start = 15.dp),
-        onClick = { onClick?.invoke() },
+        onClick = { onClick.invoke() },
         colors = ButtonDefaults.buttonColors(Color.Green)
     ) {
         Text(text)
