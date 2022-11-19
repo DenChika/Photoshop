7e2ef8757dae35421e4810fbec86f541ee7d5203
ClwnYeti <crazyyeti@mail.ru>
2022-11-19 15:29:29 +0300

    feat: add gamma converter skeleton

---
+++ src/main/kotlin/Converters/GammaConverter.kt
@@ -0,0 +1,12 @@
+package Converters
+
+class GammaConverter {
+    companion object {
+        fun ChangeForVisualization(shapeValues : FloatArray, oldValue: Float, newValue: Float) : FloatArray {
+            TODO("Add implementation")
+        }
+        fun ChangeForFile(shapeValues : FloatArray, oldValue: Float, newValue: Float) : FloatArray {
+            TODO("Add implementation")
+        }
+    }
+}
