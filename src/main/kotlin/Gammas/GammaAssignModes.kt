3a3593c829f97c1f4e02983e7348a71cce0b0e9a
ClwnYeti <crazyyeti@mail.ru>
2022-11-22 22:49:54 +0300

    feat: add gamma assign mode enum

---
+++ src/main/kotlin/Gammas/GammaAssignModes.kt
@@ -0,0 +1,23 @@
+package Gammas
+
+import Configurations.AppConfiguration
+import Converters.GammaConverter
+
+enum class GammaAssignModes {
+    SRGB {
+        override fun Apply(shapeValues: FloatArray) : FloatArray {
+            return GammaConverter.CovertToSRGB(shapeValues)
+        }
+
+    },
+    Custom {
+        override fun Apply(shapeValues: FloatArray) : FloatArray{
+            return GammaConverter.ConvertToVisualize(shapeValues, AppConfiguration.Gamma.AssignCustomValue)
+        }
+
+    };
+    abstract fun Apply(shapeValues: FloatArray) : FloatArray
+    fun GetName(): String {
+        return this.name
+    }
+}
