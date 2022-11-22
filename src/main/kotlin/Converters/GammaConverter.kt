010509ce9417efb601d00491fdc5e64acbb6b410
ClwnYeti <crazyyeti@mail.ru>
2022-11-22 22:50:49 +0300

    feat: add gamma srgb convert func without implementation

--- src/main/kotlin/Converters/GammaConverter.kt
+++ src/main/kotlin/Converters/GammaConverter.kt
@@ -2,11 +2,17 @@ package Converters
 
 class GammaConverter {
     companion object {
-        fun ChangeForVisualization(shapeValues : FloatArray, oldValue: Float, newValue: Float) : FloatArray {
-            TODO("Add implementation")
+        fun ConvertToVisualize(shapeValues : FloatArray, gammaValue: Float) : FloatArray {
+            return shapeValues
+            //TODO("Add implementation")
         }
-        fun ChangeForFile(shapeValues : FloatArray, oldValue: Float, newValue: Float) : FloatArray {
-            TODO("Add implementation")
+        fun CovertToSRGB(shapeValues : FloatArray) : FloatArray {
+            return shapeValues
+            //TODO("Add implementation")
+        }
+        fun ConvertToSave(shapeValues : FloatArray, oldValue: Float, newValue: Float) : FloatArray {
+            return shapeValues
+            //TODO("Add implementation")
         }
     }
 }
