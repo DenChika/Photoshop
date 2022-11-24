f2266c0dcb71f36d7a146e6a560a6b60db7f9f7b
GTEgorss <noartu@gmail.com>
2022-11-25 01:40:17 +0300

    feat: added custom and sRGB conversion implementation

--- src/main/kotlin/Converters/GammaConverter.kt
+++ src/main/kotlin/Converters/GammaConverter.kt
@@ -1,14 +1,26 @@
 package Converters
 
+import kotlin.math.pow
+
 class GammaConverter {
     companion object {
         fun ConvertToVisualize(shapeValues : FloatArray, gammaValue: Float) : FloatArray {
+            for (i in 0..2) {
+                shapeValues[i] = shapeValues[i].pow(2.2f / gammaValue)
+            }
+
             return shapeValues
-            //TODO("Add implementation")
         }
         fun ConvertToSRGB(shapeValues : FloatArray) : FloatArray {
+            for (i in 0..2) {
+                if (shapeValues[i] <= 0.0031308f) {
+                    shapeValues[i] = (12.92f * shapeValues[i]).pow(2.2f)
+                } else {
+                    shapeValues[i] = ((1f + 0.055f) * shapeValues[i].pow(1 / 2.4f) - 0.055f).pow(2.2f)
+                }
+            }
+
             return shapeValues
-            //TODO("Add implementation")
         }
         fun ConvertToSave(shapeValues : FloatArray, oldValue: Float, newValue: Float) : FloatArray {
             return shapeValues
