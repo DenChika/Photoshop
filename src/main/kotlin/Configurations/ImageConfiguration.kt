e97bf5d6ab6d358873a8b429ee8ed1f5696e7ea7
GTEgorss <noartu@gmail.com>
2022-11-25 22:47:54 +0300

    feat: added drawLine function

--- src/main/kotlin/Configurations/ImageConfiguration.kt
+++ src/main/kotlin/Configurations/ImageConfiguration.kt
@@ -5,6 +5,7 @@ import ColorSpaces.ColorSpaceInstance
 import Converters.Bitmap
 import Converters.GammaConverter
 import Formats.Format
+import LinePainterHelpers.ColorMixer
 import LinePainterHelpers.OffsetCounter
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.height
@@ -20,6 +21,10 @@ import androidx.compose.ui.input.pointer.onPointerEvent
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import java.awt.image.BufferedImage
+import kotlin.math.abs
+import kotlin.math.min
+import kotlin.math.round
+import kotlin.math.sqrt
 
 class ImageConfiguration(
     _format: Format,
@@ -73,8 +78,62 @@ class ImageConfiguration(
             )
         }
     }
-    fun drawLine() {
-        TODO("Add implementation of By algorithm")
+
+    fun distanceLinePoint(line: LineConfiguration, x: Int, y: Int): Float {
+        val start = line.Start
+        val end = line.End
+        val sqrt = sqrt((start.x - end.x) * (start.x - end.x) - (start.y - end.y) * (start.y - end.y))
+
+        return abs((end.x - start.x) * (start.y - y) - (start.x - x) * (end.y - start.y)) / sqrt
+    }
+
+    fun drawLine(line: LineConfiguration) {
+
+        var start = line.Start;
+        var end = line.End;
+        if (end.x < start.x) {
+            start = line.End
+            end = line.Start
+        }
+
+        val dx = end.x - start.x
+        val dy = end.y - end.y
+        val grad = dy / dx
+
+        var y = start.y + grad * (round(start.x) - start.x)
+
+        for (x in round(start.x).toInt()..round(end.x).toInt()) {
+            for (plotY in (y - (line.Thickness - 1) / 2).toInt()..(y - (line.Thickness - 1) / 2 + line.Thickness).toInt()) {
+                val newColor = ColorMixer.Mix(pixels[plotY * width + x].GetFloatArrayOfValues(), line.GetColor(),
+                    line.MaxSaturation * min(1.0f, ((line.Thickness + 0.5f) / 2.0f - abs(y - plotY))))
+
+                pixels[plotY * width + x].UpdateValues(newColor)
+            }
+
+            y += grad
+        }
+
+        for (plotX in (round(start.x) - line.Thickness / 2).toInt()..(round(start.x) - 1).toInt()) {
+            y = start.y + grad * (plotX - start.x)
+
+            for (plotY in (y - (line.Thickness - 1) / 2.0).toInt()..(y - (line.Thickness - 1) / 2.0 + line.Thickness).toInt()) {
+                val newColor = ColorMixer.Mix(pixels[plotY * width + plotX].GetFloatArrayOfValues(), line.GetColor(),
+                    line.MaxSaturation * min(1.0f, ((line.Thickness + 0.5f) / 2.0f - distanceLinePoint(line, plotX, plotY))))
+
+                pixels[plotY * width + plotX].UpdateValues(newColor)
+            }
+        }
+
+        for (plotX in (round(end.x) + 1).toInt()..(round(end.x) + line.Thickness / 2).toInt()) {
+            y = start.y + grad * (plotX - start.x)
+
+            for (plotY in (y - (line.Thickness - 1) / 2.0).toInt()..(y - (line.Thickness - 1) / 2.0 + line.Thickness).toInt()) {
+                val newColor = ColorMixer.Mix(pixels[plotY * width + plotX].GetFloatArrayOfValues(), line.GetColor(),
+                    line.MaxSaturation * min(1.0f, ((line.Thickness + 0.5f) / 2.0f - distanceLinePoint(line, plotX, plotY))))
+
+                pixels[plotY * width + plotX].UpdateValues(newColor)
+            }
+        }
     }
 
 
@@ -95,6 +154,7 @@ class ImageConfiguration(
                             val position = OffsetCounter.getActualOffset(it.changes.first().position)
                             if (OffsetCounter.checkOffSetValidity(position)) {
                                 AppConfiguration.Line.Start = position
+                                println(position)
                             }
                         }
                     }
@@ -104,7 +164,8 @@ class ImageConfiguration(
                             val position = OffsetCounter.getActualOffset(it.changes.first().position)
                             if (OffsetCounter.checkOffSetValidity(position)) {
                                 AppConfiguration.Line.End = position
-                                drawLine()
+                                println(position)
+                                drawLine(AppConfiguration.Line)
                             }
                         }
                     },
