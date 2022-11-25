ae86760b4681ae6472de2d3c2fb63e486f815b67
ClwnYeti <crazyyeti@mail.ru>
2022-11-25 04:45:44 +0300

    feat: add validator of offset

--- src/main/kotlin/LinePainterHelpers/OffsetCounter.kt
+++ src/main/kotlin/LinePainterHelpers/OffsetCounter.kt
@@ -5,13 +5,18 @@ import androidx.compose.ui.geometry.Offset
 
 class OffsetCounter {
     companion object {
-        fun getActualOffset(offset: Offset) : Offset
-        {
+        fun getActualOffset(offset: Offset) : Offset {
             val maxVisualisationOffset = Offset(
                 minOf(1500, AppConfiguration.Image.width).toFloat(),
                 minOf(900, AppConfiguration.Image.height).toFloat())
             return Offset(offset.x / maxVisualisationOffset.x * AppConfiguration.Image.width,
                 offset.y / maxVisualisationOffset.y * AppConfiguration.Image.height)
+        }
+        fun checkOffSetValidity(offset: Offset) : Boolean {
+            return offset.x >= 0 &&
+                    offset.y >= 0 &&
+                    offset.x <= AppConfiguration.Image.width &&
+                    offset.y <= AppConfiguration.Image.height
         }
     }
 }
