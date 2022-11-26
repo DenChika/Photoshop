fd407be66b3e215aabf1d25f1744fe283a058241
GTEgorss <noartu@gmail.com>
2022-11-26 03:46:47 +0300

    feat: new drawLine algorithm

--- src/main/kotlin/LinePainterHelpers/Painter.kt
+++ src/main/kotlin/LinePainterHelpers/Painter.kt
@@ -1,82 +1,70 @@
 package LinePainterHelpers
 
 import ColorSpaces.ColorSpaceInstance
+import Configurations.AppConfiguration
 import Configurations.LineConfiguration
-import kotlin.math.abs
-import kotlin.math.min
-import kotlin.math.round
-import kotlin.math.sqrt
+import androidx.compose.ui.geometry.Offset
+import kotlin.math.*
 
 class Painter {
     companion object {
-        fun distanceLinePoint(line: LineConfiguration, x: Int, y: Int): Float {
-            val start = line.Start
-            val end = line.End
-            val sqrt = sqrt((start.x - end.x) * (start.x - end.x) - (start.y - end.y) * (start.y - end.y))
+        fun distanceLinePoint(start: Offset, end: Offset, x: Int, y: Int): Float {
+            val sqrt = sqrt((start.x - end.x) * (start.x - end.x) + (start.y - end.y) * (start.y - end.y))
 
-            return abs((end.x - start.x) * (start.y - y) - (start.x - x) * (end.y - start.y)) / sqrt
+            var distance = abs((end.x - start.x) * (start.y - y) - (start.x - x) * (end.y - start.y)) / sqrt
+
+            if (distance.isNaN()) {
+                distance = 0f
+            }
+
+            return distance
         }
 
-        fun drawLine(pixels: Array<ColorSpaceInstance>, width: Int, line: LineConfiguration): Array<FloatArray> {
-            var start = line.Start;
-            var end = line.End;
+        fun drawLine(pixels: Array<ColorSpaceInstance>, line: LineConfiguration) {
+            var start = line.Start
+            var end = line.End
+
+            var length = AppConfiguration.Image.width
+
+            val steep = abs(end.y - start.y) > abs(end.x - start.x)
+            if (steep) {
+                start = Offset(start.y, start.x)
+                end = Offset(end.y, end.x)
+            }
+
             if (end.x < start.x) {
-                start = line.End
-                end = line.Start
+                start = end.also { end = start }
             }
 
             val dx = end.x - start.x
-            val dy = end.y - end.y
-            val grad = dy / dx
+            val dy = end.y - start.y
 
-            var y = start.y + grad * (round(start.x) - start.x)
+            var grad = dy / dx
+
+            if (grad.isNaN()) {
+                grad = 0f
+            }
 
             for (x in round(start.x).toInt()..round(end.x).toInt()) {
-                for (plotY in (y - (line.Thickness - 1) / 2).toInt()..(y - (line.Thickness - 1) / 2 + line.Thickness).toInt()) {
-                    val newColor = ColorMixer.Mix(
-                        pixels[plotY * width + x].GetFloatArrayOfValues(), line.GetColor(),
-                        line.MaxSaturation * min(1.0f, ((line.Thickness + 0.5f) / 2.0f - abs(y - plotY)))
-                    )
+                for (plotX in ((x - line.Thickness) / 1.0f).toInt()..((x + line.Thickness) / 1.0f).toInt()) {
+                    val y = start.y + grad * (plotX - start.x)
 
-                    pixels[plotY * width + x].UpdateValues(newColor)
-                }
+                    for (plotY in ((y - line.Thickness) / 1.0f).toInt()..((y + line.Thickness + 1f) / 1.0f).toInt()) {
+                        if (steep) {
+                            val newColor = ColorMixer.Mix(
+                                pixels[plotX * length + plotY].GetFloatArrayOfValues(), line.GetColor(),
+                                line.MaxSaturation * max(0f, min(1.0f, line.Thickness / 10f / distanceLinePoint(start, end, plotX, plotY))))
 
-                y += grad
-            }
-
-            for (plotX in (round(start.x) - line.Thickness / 2).toInt()..(round(start.x) - 1).toInt()) {
-                y = start.y + grad * (plotX - start.x)
-
-                for (plotY in (y - (line.Thickness - 1) / 2.0).toInt()..(y - (line.Thickness - 1) / 2.0 + line.Thickness).toInt()) {
-                    val newColor = ColorMixer.Mix(
-                        pixels[plotY * width + plotX].GetFloatArrayOfValues(), line.GetColor(),
-                        line.MaxSaturation * min(
-                            1.0f,
-                            ((line.Thickness + 0.5f) / 2.0f - distanceLinePoint(line, plotX, plotY))
-                        )
-                    )
-
-                    pixels[plotY * width + plotX].UpdateValues(newColor)
+                            pixels[plotX * length + plotY].UpdateValues(newColor)
+                        } else {
+                            val newColor = ColorMixer.Mix(
+                                pixels[plotY * length + plotX].GetFloatArrayOfValues(), line.GetColor(),
+                                line.MaxSaturation * max(0f, min(1.0f, line.Thickness / 10f / distanceLinePoint(start, end, plotX, plotY))))
+                            pixels[plotY * length + plotX].UpdateValues(newColor)
+                        }
+                    }
                 }
             }
-
-            for (plotX in (round(end.x) + 1).toInt()..(round(end.x) + line.Thickness / 2).toInt()) {
-                y = start.y + grad * (plotX - start.x)
-
-                for (plotY in (y - (line.Thickness - 1) / 2.0).toInt()..(y - (line.Thickness - 1) / 2.0 + line.Thickness).toInt()) {
-                    val newColor = ColorMixer.Mix(
-                        pixels[plotY * width + plotX].GetFloatArrayOfValues(), line.GetColor(),
-                        line.MaxSaturation * min(
-                            1.0f,
-                            ((line.Thickness + 0.5f) / 2.0f - distanceLinePoint(line, plotX, plotY))
-                        )
-                    )
-
-                    pixels[plotY * width + plotX].UpdateValues(newColor)
-                }
-            }
-
-            return Array<FloatArray>(0) { floatArrayOf() }
         }
     }
 }
