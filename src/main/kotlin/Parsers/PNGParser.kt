24d406025310d0dedf86b338ca4ab65315017854
Егор Дмитриевич Сергеев <311456@niuitmo.ru>
2023-01-05 21:35:23 +0300

    feat: add other chunks (initial)

--- src/main/kotlin/Parsers/PNGParser.kt
+++ src/main/kotlin/Parsers/PNGParser.kt
@@ -1,24 +1,25 @@
 package Parsers
 
 import Configurations.ImageConfiguration
+import Formats.PNG
 import Tools.PNGException
 
 class PNGParser {
     companion object {
 
-        const val pngStringLength = 8
-        const val chunkNameSize = 4
-        const val widthHeightSize = 4
-        const val headerLength = 13
+        private const val pngStringLength = 8
+        private const val chunkNameSize = 4
+        private const val widthHeightSize = 4
+        private const val headerLength = 13
         private val power256 = intArrayOf(256 * 256 * 256, 256 * 256, 256, 1)
 
         fun Parse(byteArray: ByteArray): ImageConfiguration? {
             var index = pngStringLength
             var foundEnd = false
-
             var headerRead = false
+            var paletteRead = false
 
-            var pngHeader : PNGHeader
+            var pngHeader: PNGHeader? = null
 
             while (!foundEnd) {
 
@@ -33,17 +34,36 @@ class PNGParser {
                             }
 
                             index += chunkNameSize
-
                             pngHeader = readHeader(byteArray, index)
+                            index += headerLength
+                            headerRead = true
 
-                            if (pngHeader.getColorType() !in intArrayOf(0, 2, 3)) {
-                                throw PNGException.wrongFormat("Unsupported color type.")
+                        } else if (
+                            byteArray[index + 1].toUByte().toInt().toChar() == 'E' &&
+                            byteArray[index + 2].toUByte().toInt().toChar() == 'N' &&
+                            byteArray[index + 3].toUByte().toInt().toChar() == 'D'
+                        ) {
+                            foundEnd = true
+
+                        } else if (
+                            byteArray[index + 1].toUByte().toInt().toChar() == 'D' &&
+                            byteArray[index + 2].toUByte().toInt().toChar() == 'A' &&
+                            byteArray[index + 3].toUByte().toInt().toChar() == 'T'
+                        ) {
+                            readData(byteArray, pngHeader)
+
+                        }
+                    } else if (byteArray[index].toUByte().toInt().toChar() == 'P') {
+                        if (byteArray[index + 1].toUByte().toInt().toChar() == 'L' &&
+                            byteArray[index + 2].toUByte().toInt().toChar() == 'T' &&
+                            byteArray[index + 3].toUByte().toInt().toChar() == 'E'
+                        ) {
+                            if (paletteRead) {
+                                throw PNGException.wrongFormat("There must not be more than one PLTE chunk.")
                             }
 
-                            index += headerLength
-
-                            headerRead = true
-                            foundEnd = true
+                            readPalette(byteArray, pngHeader)
+                            paletteRead = true
                         }
                     }
                 } catch (e: IndexOutOfBoundsException) {
@@ -80,7 +100,32 @@ class PNGParser {
 
             index += 3
 
+            if (colorType !in intArrayOf(0, 2, 3)) {
+                throw PNGException.wrongFormat("Unsupported color type.")
+            }
+
+            if (width <= 0 || height <= 0) {
+                throw PNGException.wrongFormat("Width and height have to be positive.")
+            }
+
+            if (bitDepth !in intArrayOf(1, 2, 4, 8, 16) ||
+                colorType == 2 && bitDepth !in intArrayOf(8, 16) ||
+                colorType == 3 && bitDepth !in intArrayOf(1, 2, 4, 8)
+            ) {
+                throw PNGException.wrongFormat("Invalid bit depth value: $bitDepth")
+            }
+
             return PNGHeader(width, height, bitDepth, colorType)
+        }
+
+        fun readPalette(byteArray: ByteArray, pngHeader: PNGHeader?) {
+            if (pngHeader != null && (pngHeader.getColorType() != 2 || pngHeader.getColorType() != 3)) {
+                throw PNGException.wrongFormat("Palette chunk can appear only if color type is 2 or 3.")
+            }
+        }
+
+        fun readData(byteArray: ByteArray, pngHeader: PNGHeader?) {
+
         }
     }
 }
